ee9cad6050b94e483d3846cd06dd6e0ab705d393
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2024-06-25 19:17:00 +0200

    Fix rubocop offenses

--- lib/decidim/internal_evaluation/test/factories.rb
+++ lib/decidim/internal_evaluation/test/factories.rb
@@ -6,8 +6,8 @@ require "decidim/core/test/factories"
 FactoryBot.define do
   factory :internal_evaluation_component, parent: :component do
     name { Decidim::Components::Namer.new(participatory_space.organization.available_locales, :internal_evaluation).i18n_name }
-    manifest_name :internal_evaluation
-    participatory_space { create(:participatory_process, :with_steps) }
+    manifest_name { :internal_evaluation }
+    participatory_space { association(:participatory_process, :with_steps) }
   end
 
   # Add engine factories here
